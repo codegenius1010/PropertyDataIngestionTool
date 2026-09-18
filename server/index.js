669bfbd77bef35b95760cf64bbc0b787eee2b6a7
Developer <developer@example.com>
2026-09-18 16:57:04 -0500

    Serve static dashboard from server, loosen react-scripts version constraint

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 const uploadRoutes = require('./routes/upload');
